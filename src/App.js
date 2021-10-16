78dcdb605a042d6e3055cf87bd6dec59529297e1
Olivier Chatelin <olivier.chatelin@gmail.com>
2021-10-16 10:21:06 +0200

    checkpoint card component and map on them

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
-
+import Checkpoint from "./componennts/Checkpoint";
+import {dividerClasses} from "@mui/material";
 function App() {
-    return <Button  variant="contained">Hello World</Button>;
+    const checkpoints = [
+        {
+            cursus : "Javascript",
+            title : "Checkpoint 1",
+            description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab cumque dicta doloremque ea expedita facilis non numquam porro possimus tempora?",
+        },
+        {
+            cursus : "Javascript",
+            title : "Checkpoint 2",
+            description : "Lorem ipsum ea expedita facilis non numquam porro possimus tempora?",
+        },
+        {
+            cursus : "Javascript",
+            title : "Checkpoint 3",
+            description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab cumque dicta doloremque ea expeipsum dolor sit amet, consectetur adipisicing elit. Ab cumque dicta doloremque ea expedita facilis non numquam porro possimus tempora?",
+        },
+        {
+            cursus : "PHP",
+            title : "Checkpoint 1",
+            description : "Lorem ipsum dolor sit amet, consectetur mque dicta doloremque ea expedita facilis non numquam porro possimus tempora?",
+        },
+        {
+            cursus : "PHP",
+            title : "Checkpoint 2",
+            description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab cumque dicta doloremque ea expedita facilis non numquam porro possimus tempora?",
+        },
+
+    ];
+
+    return (
+        checkpoints.filter(checkpoint => checkpoint.cursus === "Javascript")
+            .map(checkpoint =>
+            <Checkpoint cursus={checkpoint.cursus} title={checkpoint.title} description={checkpoint.description}/>
+        )
+    );
 }
+
 export default App;
---
+++ src/componennts/Checkpoint.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {Card, Typography} from "@mui/material";
+import {CardContent} from "@mui/material";
+import {CardActions} from "@mui/material";
+import {Button} from "@mui/material";
+import "../App.scss";
+
+
+function Checkpoint(props) {
+    return(
+        <Card sx={{maxWidth: 400, margin: 5}}>
+            <CardContent>
+                <Typography sx={{fontsize: 14}} color="text.secondary" gutterBottom>
+                    {props.cursus}
+                </Typography>
+                <Typography variant="h5" component="div">
+                    {props.title}
+                </Typography>
+                <Typography variant="body2">
+                    {props.description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Compléter</Button>
+            </CardActions>
+        </Card>
+
+    );
+}
+export default Checkpoint;
